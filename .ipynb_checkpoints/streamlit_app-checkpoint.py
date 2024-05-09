605e69e74e1d3a8ab960c52f831fb83dd06e62f0
Unurbold99 <m.unurbold@letumongolia.com>
2024-05-09 10:19:08 +0800

    dd

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -1,41 +1,42 @@
 import streamlit as st
-import numpy as np
 from PIL import Image
-import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
-import joblib
-import torch
+from tensorflow.keras.applications.vgg16 import decode_predictions
 from fastai.vision.all import *
 
-# Load the trained model
-model = joblib.load('x_ray_pred.pkl')
+# Load the model
+model = load_model('model.h5')
+
+# Define a function to make predictions
+def make_prediction(image):
+    # Load the image
+    img = load_img(image, target_size=(224, 224))
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    predictions = model.predict(img_array)
+    predictions = decode_predictions(predictions, top=3)[0]
+    return predictions
+
+# Define the class labels
+class_labels = ["Atelectasis", "Cardiomegaly", "Effusion", "Infiltration",
+                 "Mass", "Nodule", "Pneumonia", "Pneumothorax",
+                 "Consolidation", "No Finding"]
 
 # Create the Streamlit app
-st.title("X-Ray Classification App")
-st.write("Upload an X-ray image to make a prediction:")
+st.title("Image Classifier")
+st.write("Upload an image to classify it")
 
 # Create a file uploader
-image_file = st.file_uploader("Select an image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-# Create a button to trigger the prediction
-if st.button("Make Prediction"):
-    if image_file is not None:
-
-        image = Image.open(image_file)
-
-        image = np.array(image)
-
-        image = image.resize((320, 320))
-
-        image = np.array(image)
-        predictions = model.predict(image)
-
-        class_labels = ["Atelectasis", "Cardiomegaly", "Effusion", "Infiltration",
-                          "Mass", "Nodule", "Pneumonia", "Pneumothorax",
-                          "Consolidation", "No Finding"]
-        predicted_class_labels = [class_labels[i] for i in np.argmax(predictions, axis=1)[0]]
-
-        st.write("Predicted class labels:", predicted_class_labels)
-    else:
-        st.write("Please upload an image.")
+# Create a button to make the prediction
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    predictions = make_prediction(uploaded_file)
+    st.write("Predictions:")
+    for prediction in predictions:
+        st.write(f"{class_labels[int(prediction[1]) - 1]}: {prediction[2]}")

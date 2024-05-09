85ab115ef48dbb2e1ceed09009a3d9247666c89b
Unurbold99 <m.unurbold@letumongolia.com>
2024-05-09 14:25:47 +0800

    bb

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tensorflow as tf
-from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import joblib
 import torch

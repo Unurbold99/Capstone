90073cc6f4dfb731b828aa5de8211dea76de81b7
Unurbold99 <m.unurbold@letumongolia.com>
2024-05-09 13:18:23 +0800

    dd

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -10,7 +10,7 @@ from fastai.vision.all import *
 import pickle
 
 # Load the model
-learn = load_learner('model_3.pkl')
+learn = load_learner('export.pkl')
 
 # Create the Streamlit app
 st.title("X-Ray Classification App")
